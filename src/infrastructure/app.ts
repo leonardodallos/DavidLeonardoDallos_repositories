9b1b46063de8aa41b97eca35e62bed3f26ef0232
Leonardo Dallos <leonardodallos@hotmail.com>
2022-07-13 17:52:10 -0500

    Add body-parser, cors and helmet dependencies

--- src/infrastructure/app.ts
+++ src/infrastructure/app.ts
@@ -1,9 +1,17 @@
 import express from 'express'
-import repositories from './routes/repositories';
+import cors from 'cors'
+import helmet from 'helmet'
+import * as bodyParser from 'body-parser'
+
 import RespotoriesRouter from './routes/repositories'
 
 const app = express();
+app.use(cors())
+app.use(helmet())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
+// ROUTER
 new RespotoriesRouter(app)
 
 export default app;
